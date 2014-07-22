acacd42233fa1b98434662fa0baaea6128107f1b
Steven Nemetz <snemetz@book.com>
2014-07-22 19:50:51 +0000

    Add missing coverage upload. Cleanup format of rspec

--- spec/defines/dripstat_application_monitoring_spec.rb
+++ spec/defines/dripstat_application_monitoring_spec.rb
@@ -3,112 +3,93 @@
 require 'spec_helper'
 
 describe 'dripstat::application_monitoring' do
-
   let (:title) { 'dripstat application monitoring' }
-
   context 'without a version specified' do
-
-		it {
+    it {
       expect { subject }.to raise_error(
         Puppet::Error, /The version of the DripStat agent must be provided/
     )}
-
-	end
+  end
 
   context 'without an installation root directory specified' do
-
     let (:params) { { :dripstat_version => '3.5.0' } }
-
-		it {
+    it {
       expect { subject }.to raise_error(
         Puppet::Error, /The root directory of the application server installation must be provided/
     )}
-
-	end
+  end
 
   context 'without a license_key specified' do
-
-		let (:params) { { :dripstat_version => '3.5.0', :dripstat_app_root_dir => '/opt/appserver' } }
-
+    let (:params) { { :dripstat_version => '3.5.0', :dripstat_app_root_dir => '/opt/appserver' } }
     it {
-    	expect { subject }.to raise_error(
+      expect { subject }.to raise_error(
       	Puppet::Error, /The license key associated with your DripStat account must be provided/
-  	)}
-
+    )}
   end
 
   context 'without a valid agent loglevel' do
-
-		let (:params) { { :dripstat_version => '3.5.0',
-                      :dripstat_app_root_dir => '/opt/appserver',
-											:dripstat_license_key => '1234567890' ,
-											:dripstat_agent_loglevel => 'vverbosedebug'} }
-
-		it {
+    let (:params) { { :dripstat_version        => '3.5.0',
+                      :dripstat_app_root_dir   => '/opt/appserver',
+                      :dripstat_license_key    => '1234567890' ,
+                      :dripstat_agent_loglevel => 'vverbosedebug'} }
+    it {
     	expect { subject }.to raise_error(
       	Puppet::Error, /vverbosedebug is not one of valid predefined values for agent loglevels/
-  	)}
-
-	end
+    )}
+  end
 
   context 'without a valid use_ssl value' do
-
-		let (:params) { { :dripstat_version => '3.5.0',
-                      :dripstat_app_root_dir => '/opt/appserver',
-                      :dripstat_license_key => '1234567890' ,
+    let (:params) { { :dripstat_version        => '3.5.0',
+                      :dripstat_app_root_dir   => '/opt/appserver',
+                      :dripstat_license_key    => '1234567890' ,
                       :dripstat_agent_loglevel => 'fine',
-											:dripstat_use_ssl => 'bla'} }
+                      :dripstat_use_ssl        => 'bla'} }
 
-		it {
+    it {
       expect { subject }.to raise_error(
         Puppet::Error, /parameter dripstat_use_ssl must be a boolean/
     )}
+  end
 
-	end
-
-	context 'with a valid version, installation directory and license_key' do
-
-		let (:params) { { :dripstat_version => '3.5.0',
+  context 'with a valid version, installation directory and license_key' do
+    let (:params) { { :dripstat_version      => '3.5.0',
                       :dripstat_app_root_dir => '/opt/appserver',
-											:dripstat_license_key => '1234567890' } }
+                      :dripstat_license_key  => '1234567890' } }
 
-		it { should contain_file '/opt/appserver/dripstat' }
-		it { should contain_file '/opt/appserver/dripstat/dripstat.jar' }
-		it { should contain_file '/opt/appserver/dripstat/config.properties' }
+    it { should contain_file '/opt/appserver/dripstat' }
+    it { should contain_file '/opt/appserver/dripstat/dripstat.jar' }
+    it { should contain_file '/opt/appserver/dripstat/config.properties' }
+  end
 
-	end
+  context 'with a valid version, installation directory, application owner and group, license_key record sql option and agent loglevel' do
+    let (:params) { { :dripstat_version        => '3.5.0',
+                      :dripstat_app_root_dir   => '/opt/appserver',
+                      :dripstat_app_owner      => 'dripstat',
+                      :dripstat_app_group      => 'dripstat',
+                      :dripstat_license_key    => '1234567890',
+                      :dripstat_agent_loglevel => 'finer' } }
 
-	context 'with a valid version, installation directory, application owner and group, license_key record sql option and agent loglevel' do
-
-		let (:params) { { :dripstat_version => '3.5.0',
-                      :dripstat_app_root_dir => '/opt/appserver',
-		:dripstat_app_owner => 'dripstat',
-		:dripstat_app_group => 'dripstat',
-		:dripstat_license_key => '1234567890',
-		:dripstat_agent_loglevel => 'finer' } }
-
-		it do
+    it do
     	should contain_file('/opt/appserver/dripstat').with({
       	'ensure' => 'directory',
       	'owner'  => 'dripstat',
       	'group'  => 'dripstat',
     	})
-  	end
-		it do
+    end
+    it do
       should contain_file('/opt/appserver/dripstat/dripstat.jar').with({
         'ensure' => 'file',
         'owner'  => 'dripstat',
         'group'  => 'dripstat',
       })
     end
-		it do
+    it do
       should contain_file('/opt/appserver/dripstat/config.properties').with({
         'ensure' => 'file',
         'owner'  => 'dripstat',
         'group'  => 'dripstat',
       })
     end
-
-	end
+  end
 
 end
